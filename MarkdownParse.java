f6f9fc3564aa665811d0ddc73d9e62cfed667250
Adironene <49921653+adironene@users.noreply.github.com>
2022-04-14 12:45:31 -0700

    check for if it is an image link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,14 +13,22 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
             System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(openBracket ==-1 || closeBracket==-1 || openParen ==-1 || closeParen==-1)
+            System.out.println(openBracket);
+            System.out.println(closeBracket);
+            System.out.println(openParen);
+            System.out.println(closeParen);
+            if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1)
                 break;
+            if (openBracket != 0 && markdown.substring(openBracket - 1, openBracket).equals("!")){
+                currentIndex = closeParen + 1;
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
@@ -28,11 +36,10 @@ public class MarkdownParse {
         return toReturn;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
---
+++ testImage.md
@@ -0,0 +1,3 @@
+![image](https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/640px-Cat03.jpg)
+[link1](https://something.com)
+![image](https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/640px-Cat03.jpg)
