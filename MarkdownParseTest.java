8d32aa80865068c77353ad1f9609222f74dc3313
Adironene <49921653+adironene@users.noreply.github.com>
2022-04-28 12:21:34 -0700

    commented out tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,48 +8,48 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
+    // @Test
+    // public void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
 
-    @Test
-    public void testFile() throws IOException {
-        Path fileName = Path.of("test-file.md");
-        String content = Files.readString(fileName);
-        MarkdownParse m = new MarkdownParse();
-        ArrayList<String> links = m.getLinks(content);
-        ArrayList<String> correct_links = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
-        assertEquals(links, correct_links);
-    }
+    // @Test
+    // public void testFile() throws IOException {
+    //     Path fileName = Path.of("test-file.md");
+    //     String content = Files.readString(fileName);
+    //     MarkdownParse m = new MarkdownParse();
+    //     ArrayList<String> links = m.getLinks(content);
+    //     ArrayList<String> correct_links = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
+    //     assertEquals(links, correct_links);
+    // }
 
-    @Test
-    public void test2() throws IOException {
-        Path fileName = Path.of("test2.md");
-        String content = Files.readString(fileName);
-        MarkdownParse m = new MarkdownParse();
-        ArrayList<String> links = m.getLinks(content);
-        ArrayList<String> correct_links = new ArrayList<>(Arrays.asList("https://something.com"));
-        assertEquals(links, correct_links);
-    }
+    // @Test
+    // public void test2() throws IOException {
+    //     Path fileName = Path.of("test2.md");
+    //     String content = Files.readString(fileName);
+    //     MarkdownParse m = new MarkdownParse();
+    //     ArrayList<String> links = m.getLinks(content);
+    //     ArrayList<String> correct_links = new ArrayList<>(Arrays.asList("https://something.com"));
+    //     assertEquals(links, correct_links);
+    // }
 
-    @Test
-    public void test3() throws IOException {
-        Path fileName = Path.of("test3.md");
-        String content = Files.readString(fileName);
-        MarkdownParse m = new MarkdownParse();
-        ArrayList<String> links = m.getLinks(content);
-        ArrayList<String> correct_links = new ArrayList<>(Arrays.asList());
-        assertEquals(links, correct_links);
-    }
+    // @Test
+    // public void test3() throws IOException {
+    //     Path fileName = Path.of("test3.md");
+    //     String content = Files.readString(fileName);
+    //     MarkdownParse m = new MarkdownParse();
+    //     ArrayList<String> links = m.getLinks(content);
+    //     ArrayList<String> correct_links = new ArrayList<>(Arrays.asList());
+    //     assertEquals(links, correct_links);
+    // }
 
-    @Test
-    public void testImage() throws IOException {
-        Path fileName = Path.of("testImage.md");
-        String content = Files.readString(fileName);
-        MarkdownParse m = new MarkdownParse();
-        ArrayList<String> links = m.getLinks(content);
-        ArrayList<String> correct_links = new ArrayList<>(Arrays.asList());
-        assertEquals(links, correct_links);
-    }
+    // @Test
+    // public void testImage() throws IOException {
+    //     Path fileName = Path.of("testImage.md");
+    //     String content = Files.readString(fileName);
+    //     MarkdownParse m = new MarkdownParse();
+    //     ArrayList<String> links = m.getLinks(content);
+    //     ArrayList<String> correct_links = new ArrayList<>(Arrays.asList());
+    //     assertEquals(links, correct_links);
+    // }
 }
