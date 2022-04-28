2ac5b9fcdb1d8951a5955a11f22e4d720d037ce2
Adironene <49921653+adironene@users.noreply.github.com>
2022-04-28 12:29:38 -0700

    changed test file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,10 +8,10 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MarkdownParseTest {
-    // @Test
-    // public void addition() {
-    //     assertEquals(2, 1 + 1);
-    // }
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
 
     // @Test
     // public void testFile() throws IOException {
